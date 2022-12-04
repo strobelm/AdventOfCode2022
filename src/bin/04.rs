e93f8e7a012def73338827dcb2567fc9e76e6d17
Michael Strobel <m.strobel@posteo.de>
2022-12-04 14:35:20 +0100

    AOC: simplify

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,31 +1,25 @@
-use core::cmp::{max, min};
-use core::ops::Range;
 use itertools::Itertools;
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let range_pairs = parse_ranges(input);
-    let count = range_pairs
-        .iter()
-        .map(|t| ranges_contain_each_other(*t))
-        .filter(|b| *b)
+    let count = input
+        .lines()
+        .filter(|line| {
+            let (a, b) = line.split(',').map(MyRange::from).next_tuple().unwrap();
+            a.is_subrange(b) || b.is_subrange(a)
+        })
         .count();
-
     Some(count as u32)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    let range_pairs = parse_ranges(input);
-    let number_of_intersections: u32 = range_pairs
-        .iter()
-        .map(|t| {
-            let a = t.0.start..t.0.end;
-            let b = t.1.start..t.1.end;
-            intersect(&a, &b)
+    let number_of_intersections = input
+        .lines()
+        .filter(|line| {
+            let (a, b) = line.split(',').map(MyRange::from).next_tuple().unwrap();
+            a.is_overlapping(b) || b.is_overlapping(a)
         })
-        .filter(|r| !r.is_empty())
-        .count() as u32;
-
-    Some(number_of_intersections)
+        .count();
+    Some(number_of_intersections as u32)
 }
 
 fn main() {
@@ -35,48 +29,30 @@ fn main() {
 }
 
 #[derive(Copy, Clone, Debug)]
-pub struct StartEnd {
-    pub start: u32,
-    pub end: u32,
+struct MyRange {
+    start: usize,
+    end: usize,
 }
-pub type RangeTuple = (std::ops::Range<u32>, std::ops::Range<u32>);
-fn parse_ranges(input: &str) -> Vec<(StartEnd, StartEnd)> {
-    let mut vec = vec![];
-    for line in input.lines() {
-        let records = line.split(',');
-        let pairs: _ = records
-            .map(|r| {
-                let start_end: _ = r
-                    .split('-')
-                    .map(|x| x.parse::<u32>().unwrap())
-                    .collect::<Vec<u32>>();
-                let (start, end) = start_end.iter().copied().collect_tuple().unwrap();
 
-                StartEnd { start, end }
-            })
-            .collect::<Vec<StartEnd>>();
-
-        vec.push((pairs[0], pairs[1]))
+impl MyRange {
+    fn is_overlapping(self, other: MyRange) -> bool {
+        self.start >= other.start && self.start <= other.end
+            || self.end >= other.start && self.end <= other.end
     }
-    vec
+
+    fn is_subrange(self, other: MyRange) -> bool {
+        self.start >= other.start && self.end <= other.end
+    }
 }
 
-pub fn ranges_contain_each_other(tuple: (StartEnd, StartEnd)) -> bool {
-    let (fst, scd) = tuple;
-    let fst_range = fst.start..fst.end + 1;
-    let scd_range = scd.start..scd.end + 1;
-
-    let fst_contains_scd = fst_range.contains(&scd.start) && fst_range.contains(&scd.end);
-    let scd_contains_fst = scd_range.contains(&fst.start) && scd_range.contains(&fst.end);
-
-    fst_contains_scd || scd_contains_fst
-}
-
-fn intersect(a: &Range<u32>, b: &Range<u32>) -> Range<u32> {
-    let start = max(a.start, b.start);
-    let end = min(a.end, b.end);
-
-    start..end + 1
+impl From<&str> for MyRange {
+    fn from(input: &str) -> Self {
+        let (start, end) = input.split_once('-').unwrap();
+        MyRange {
+            start: start.parse().unwrap(),
+            end: end.parse().unwrap(),
+        }
+    }
 }
 
 #[cfg(test)]

5304e921918908fba94dbe1d12bd421b884c26db
Michael Strobel <m.strobel@posteo.de>
2022-12-06 10:35:59 +0100

    AOC: day 6

---
+++ src/bin/06.rs
@@ -0,0 +1,49 @@
+pub fn part_one(input: &str) -> Option<u32> {
+    Some(find_index(input, 4) as u32)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let idx = find_index(input, 14);
+    Some(idx as u32)
+}
+
+fn find_index(input: &str, offset: usize) -> usize {
+    input
+        .char_indices()
+        .position(|(i, _)| all_different(&input[i..i + offset]))
+        .unwrap()
+        + offset
+}
+
+fn all_different(input: &str) -> bool {
+    for (i, c) in input.chars().enumerate() {
+        let sub: String = input[i + 1..].try_into().unwrap();
+        if sub.contains(c) {
+            return false;
+        }
+    }
+    true
+}
+
+fn main() {
+    let input = &advent_of_code::read_file("inputs", 6);
+    advent_of_code::solve!(1, part_one, input);
+    advent_of_code::solve!(2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = advent_of_code::read_file("examples", 6);
+        assert_eq!(part_one(&input), Some(7));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = advent_of_code::read_file("examples", 6);
+        assert_eq!(part_two(&input), Some(19));
+    }
+}

25ae3ac148d6dfb01961ef8ce5726740ce14bcae
Michael Strobel <m.strobel@posteo.de>
2022-12-07 12:02:01 +0100

    AOC: day 7

---
+++ src/bin/07.rs
@@ -0,0 +1,66 @@
+pub fn part_one(input: &str) -> Option<u32> {
+    let sum = parse(&mut input.lines())
+        .into_iter()
+        .filter(|&s| s <= 100_000)
+        .sum();
+    Some(sum)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let filesystem_size = 70_000_000;
+    let needed_size = 30_000_000;
+
+    let mut subdirs = parse(&mut input.lines());
+
+    subdirs.sort();
+    let total_dir_size = subdirs.pop().unwrap();
+
+    let needed = needed_size + total_dir_size - filesystem_size;
+    let min = subdirs.iter().find(|&d| d >= &needed).unwrap();
+
+    Some(*min)
+}
+
+fn parse<'a>(input: &mut impl Iterator<Item = &'a str>) -> Vec<u32> {
+    let mut total = 0;
+    let mut subdirs = vec![];
+    loop {
+        match input.next() {
+            Some("$ cd ..") | None => break,
+            Some(l) if l.starts_with("dir") || l.starts_with("$ ls") => (),
+            Some(l) if l.starts_with("$ cd") => {
+                subdirs.append(&mut parse(input));
+                total += subdirs.last().unwrap();
+            }
+            Some(l) => {
+                let (size, _) = l.split_once(' ').unwrap();
+                total += size.parse::<u32>().unwrap();
+            }
+        }
+    }
+    subdirs.push(total);
+    subdirs
+}
+
+fn main() {
+    let input = &advent_of_code::read_file("inputs", 7);
+    advent_of_code::solve!(1, part_one, input);
+    advent_of_code::solve!(2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = advent_of_code::read_file("examples", 7);
+        assert_eq!(part_one(&input), Some(95437));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = advent_of_code::read_file("examples", 7);
+        assert_eq!(part_two(&input), Some(24933642));
+    }
+}

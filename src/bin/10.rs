0c84d7cd98b3307a86d8d1b8d3c07f29c2206417
Michael Strobel <m.strobel@posteo.de>
2022-12-10 19:00:57 +0100

    AOC: small refactoring

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -1,5 +1,7 @@
 use std::collections::VecDeque;
 
+use itertools::Itertools;
+
 pub fn part_one(input: &str) -> Option<i32> {
     let mut cpu = Cpu::new(input);
     let cycles = cpu.stack.len();
@@ -22,24 +24,31 @@ pub fn part_one(input: &str) -> Option<i32> {
     Some(sum)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
+pub fn part_two(input: &str) -> Option<String> {
     let mut cpu = Cpu::new(input);
 
-    let mut cyc = 1;
-    while !cpu.stack.is_empty() {
-        cpu.tick();
-        if (cpu.reg - 1..=cpu.reg + 1).contains(&cyc) {
-            print!("#")
-        } else {
-            print!(".");
-        }
-        if cyc % 40 == 0 {
-            println!();
-            cyc = 0;
-        }
-        cyc += 1;
-    }
-    None
+    let cycles = 1..=cpu.stack.len();
+
+    let res: Vec<String> = cycles
+        .chunks(40)
+        .into_iter()
+        .map(|c| {
+            return c
+                .into_iter()
+                .enumerate()
+                .map(|(i, _)| {
+                    cpu.tick();
+                    if (cpu.reg - 1..=cpu.reg + 1).contains(&((i + 1) as i32)) {
+                        return "#";
+                    } else {
+                        return ".";
+                    }
+                })
+                .collect();
+        })
+        .collect();
+
+    Some(res.into_iter().collect())
 }
 
 #[derive(Debug)]
@@ -91,6 +100,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 10);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some("#..##..##..##..##..##..##..##..##..##...##...###...###...###...###...###...###..###....####....####....####....####.....####.....#####.....#####.....#####......#####......######......######......####.######.......#######.......#######......".to_string()));
     }
 }
